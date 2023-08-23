667f432440b3d01d603def6c8b6a5d736f614c03
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-08-23 12:30:27 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,29 @@
+##stop=int(input('enter the value'))
+##for i in range(stop):
+##    print(i)
+##task:2
+##stop=int(input('enter the value'))
+##for i in range(2,stop,2):
+##    print(i)
+##task:3
+##stop=int(input('entr your value'))
+##for i in range(1,stop,2):
+##    print(i)
+##task:4
+##stop=int(input('enter the stop value'))
+##name=str(input('enter the name'))
+##num=int(input('enter the number'))
+##for i in range(1,stop):
+##    if(i%num==0):
+##        print(name)
+##    else:
+##        print(i)
+##task:5
+##stop=int(input('enter the value'))
+##table=int(input('enter the table'))
+##for i in range(1,stop):
+##    print(i,'x',table,'=',i*table)
+    
+        
+            
+
